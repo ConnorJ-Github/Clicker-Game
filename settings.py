8f29965ef0ca6a62fdf3c532e5c452c06d1e174c
CJenkins <c.e.jenkins001@gmail.com>
2024-04-29 15:56:11 +0100

    First Button

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ from pygame.math import Vector2 as vector
 
 FPS = 60 #Window FPS
 
-WIDTH, HEIGHT = 500, 500 #Controls the width and height on the game window.
+WIDTH, HEIGHT = 1200, 800 #Controls the width and height on the game window.
--- world.py
+++ world.py
@@ -1,9 +1,18 @@
 from settings import *
+import button
 
 class World:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
 
+        play_img = pygame.image.load('Assets\Play_Button.png')
+        play_img = pygame.transform.scale(play_img, (150,150))
+        self.play_button = button.Button(230,220,play_img)
+                
+
 
     def run(self):
         self.display_surface.fill('white') #Window Colour
+
+        if self.play_button.draw(self.display_surface):
+            print("click")
