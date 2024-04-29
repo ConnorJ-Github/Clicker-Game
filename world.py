c5443359df999fc9f700090c7905e68df36220de
CJenkins <c.e.jenkins001@gmail.com>
2024-04-29 16:24:38 +0100

    Button Positions
    Fixing Positioning of the buttons

--- world.py
+++ world.py
@@ -6,24 +6,24 @@ class World:
         self.display_surface = pygame.display.get_surface()
 
         coin1_img = pygame.image.load('Assets\Coin_Buttons\Info_Coin_1.png')
-        coin1_img = pygame.transform.scale(coin1_img, (150,150))
-        self.coin_one = button.Button(230,220,coin1_img)
+        coin1_img = pygame.transform.scale(coin1_img, (75,75))
+        self.coin_one = button.Button(30,25,coin1_img)
 
         coin2_img = pygame.image.load('Assets\Coin_Buttons\Info_Coin_2.png')
-        coin2_img = pygame.transform.scale(coin2_img, (150,150))
-        self.coin_two = button.Button(230,240,coin2_img)
+        coin2_img = pygame.transform.scale(coin2_img, (75,75))
+        self.coin_two = button.Button(30,125,coin2_img)
 
         coin3_img = pygame.image.load('Assets\Coin_Buttons\Info_Coin_3.png')
-        coin3_img = pygame.transform.scale(coin3_img, (150,150))
-        self.coin_three = button.Button(230,260,coin3_img)
+        coin3_img = pygame.transform.scale(coin3_img, (75,75))
+        self.coin_three = button.Button(30,225,coin3_img)
 
         coin4_img = pygame.image.load('Assets\Coin_Buttons\Info_Coin_4.png')
-        coin4_img = pygame.transform.scale(coin4_img, (150,150))
-        self.coin_four = button.Button(230,280,coin4_img)
+        coin4_img = pygame.transform.scale(coin4_img, (75,75))
+        self.coin_four = button.Button(30,325,coin4_img)
 
         coin5_img = pygame.image.load('Assets\Coin_Buttons\Info_Coin_5.png')
-        coin5_img = pygame.transform.scale(coin5_img, (150,150))
-        self.coin_five = button.Button(230,300,coin5_img)
+        coin5_img = pygame.transform.scale(coin5_img, (75,75))
+        self.coin_five = button.Button(30,425,coin5_img)
                 
 
 
