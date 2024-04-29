4e4fb43a28a453ef6bf666ac77da73d6c4212e10
CJenkins <c.e.jenkins001@gmail.com>
2024-04-29 19:57:40 +0100

    Progress Bars
    When a coin button is clicked, a progress bar will now start.
    
    At the end of the progress bar, the player earns coins.
    
    Improved code structure.

--- world.py
+++ world.py
@@ -1,6 +1,5 @@
 from settings import *
 import button
-import bars
 
 class World:
     def __init__(self):
@@ -14,6 +13,52 @@ class World:
         self.coin_five = button.Button(30,425,coin5_img)
 
         self.coins = 0 #Playeres current coins.
+        self.value = 0 #The values that coins have
+
+
+        #Progress bar values.
+        self.draw_black = False
+        self.draw_green = False
+        self.draw_red = False
+        self.draw_blue = False
+        self.draw_yellow = False
+
+        #Length of the progress bar
+        self.black_length = 0
+        self.green_length = 0
+        self.red_length = 0
+        self.blue_length = 0
+        self.yellow_length = 0
+
+        #pygame CE is used, so if you are experiencing errors here. That may be why.
+        #Speed at which progress is earned. 
+        self.black_speed = 0.2
+        self.green_speed = 0.1
+        self.red_speed = 0.07
+        self.blue_speed = 0.04
+        self.yellow_speed = 0.03
+
+
+
+    def draw_bars(self, colour, y_offset, draw, length, speed):
+
+        if draw and length < 400:
+            length += speed
+        elif length >= 400:
+            draw = False
+            length = 0
+            self.coins += self.value
+
+
+        pygame.draw.rect(self.display_surface, colour, [120, y_offset, 400,50 ])
+        pygame.draw.rect(self.display_surface, 'white', [125, y_offset + 5, 390,40 ])
+        pygame.draw.rect(self.display_surface, colour, [125, y_offset, length,50 ])
+
+        return length, draw
+
+        # pygame.draw.rect(self.display_surface, 'black', [120, 35, 400,50 ])
+        # pygame.draw.rect(self.display_surface, 'white', [125, 40, 390,40 ])
+        
 
 
 
@@ -27,18 +72,29 @@ class World:
 
         #Buttons
         if self.coin_one.draw(self.display_surface): 
-            print("coin 1")
-            self.coins += 1
-        
+            self.draw_black = True
+            self.value = 1
+
         if self.coin_two.draw(self.display_surface): 
-            print("coin 2")
-            self.coins += 10
+            self.draw_green = True
+            self.value = 5
 
         if self.coin_three.draw(self.display_surface): 
-            print("coin 3")
+            self.draw_red = True
+            self.value = 10
 
         if self.coin_four.draw(self.display_surface): 
-            print("coin 4")
+            self.draw_blue = True
+            self.value = 20
 
         if self.coin_five.draw(self.display_surface): 
-            print("coin 5")
+            self.draw_yellow = True
+            self.value = 50
+
+
+        #Progress bars
+        self.black_length, self.draw_black = self.draw_bars('black', 35, self.draw_black, self.black_length, self.black_speed)
+        self.green_length, self.draw_green = self.draw_bars('green', 135, self.draw_green, self.green_length, self.green_speed)
+        self.red_length, self.draw_red = self.draw_bars('red', 235, self.draw_red, self.red_length, self.red_speed)
+        self.blue_length , self.draw_blue = self.draw_bars('blue', 335, self.draw_blue, self.blue_length, self.blue_speed)
+        self.yellow_length,self.draw_yellow = self.draw_bars('yellow', 435, self.draw_yellow, self.yellow_length, self.yellow_speed)
